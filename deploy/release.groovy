8271f2348f01779372702b9a2a82c1b82f9caac9
Ibrahim Jarif <jarifibrahim@gmail.com>
2018-01-29 17:51:04 +0530

    fix(tests): Rename run_functional_tests.sh to run_js_functional_tests.sh

--- deploy/release.groovy
+++ deploy/release.groovy
@@ -19,7 +19,7 @@ def ci (){
             container('ui'){
                 sh '''
         npm install
-        HEADLESS_MODE=true ./tests/run_functional_tests.sh smokeTest
+        HEADLESS_MODE=true ./tests/run_js_functional_tests.sh smokeTest
 '''
             }
         }
